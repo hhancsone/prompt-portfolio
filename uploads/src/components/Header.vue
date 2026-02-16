ca4414a05ea612dc4660b14e12d4a1cb54100b23
hwd <703850016@qq.com>
2026-02-16 19:46:09 +0800

    Upload file: Header.vue

---
+++ uploads/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="lg:hidden fixed top-0 left-0 right-0 bg-white shadow-sm z-40">
+    <div class="flex items-center justify-between p-4">
+      <button @click="$emit('open-sidebar')" class="text-gray-500 hover:text-gray-800">
+        <i class="fa fa-bars"></i>
+      </button>
+      <h1 class="text-xl font-bold">Prompt<span class="text-indigo-500">Folio</span></h1>
+      <div class="w-5"></div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+defineEmits(['open-sidebar'])
+</script>
